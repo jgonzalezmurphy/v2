292154e9424df7679d51cdfa182bff76055f48c7
jgonzalezmurphy <44759998+jgonzalezmurphy@users.noreply.github.com>
2025-12-31 17:28:18 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,32 @@
+// Increment this version number (e.g., v2, v3, v4) whenever you update your index.html logic
+const CACHE_NAME = 'vault-v19'; 
+
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+// Installs the new version and caches the assets
 self.addEventListener('install', (e) => {
- e.waitUntil(caches.open('v1').then((c) => c.addAll(['index.html'])));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
+
+// Logic to clean up old versions of the cache so they don't take up space
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Serves files from cache if available, otherwise fetches from network
 self.addEventListener('fetch', (e) => {
- e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
